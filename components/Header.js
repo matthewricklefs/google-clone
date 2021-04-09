149a567e9b2c8ee0ddfb1f64270b5e17cf21dd4e
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-09 14:06:04 -0400

    builds button functionality in index.js by using useRef and useRouter, beings building search.js and it's associated Header component, begins styling Header component

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import { MicrophoneIcon, XIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+
+function Header() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  return (
+    <header>
+      <Image
+        src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        height={40}
+        width={120}
+        onClick={() => router.push("/")}
+        className="cursor-pointer"
+      />
+
+      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full flex-grow shadow-lg max-w-3xl items-center">
+        <input
+          ref={searchInputRef}
+          className="flex-grow w-full focus:outline-none"
+          type="text"
+        />
+        <XIcon
+          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+          onClick={() => (searchInputRef.current.value = "")}
+        />
+        <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+      </form>
+    </header>
+  );
+}
+
+export default Header;
